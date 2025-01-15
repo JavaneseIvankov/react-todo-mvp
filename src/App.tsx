9980ce48983384e6b733129b2b42551347afb28c
arundaya <runsworkspace@gmail.com>
2025-01-15 21:16:38 +0700

    chore: initialized project
    - installed dependencies
    - design system setup
    - project folder structure setup

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import './App.css';
+import './output.css';
+
+function App() {
+   return <></>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+   darkMode: 'class',
+   content: ['./index.html', './src/**/*.{ts,tsx,js,jsx}'],
+   theme: {
+      extend: {
+         borderRadius: {
+            lg: 'var(--radius)',
+            md: 'calc(var(--radius) - 2px)',
+            sm: 'calc(var(--radius) - 4px)',
+         },
+         colors: {
+            background: 'hsl(var(--background))',
+            foreground: 'hsl(var(--foreground))',
+            primary: {
+               DEFAULT: 'hsl(var(--primary))',
+               foreground: 'hsl(var(--primary-foreground))',
+               gradient: 'var(--primary-gradient)',
+            },
+            secondary: {
+               DEFAULT: 'hsl(var(--secondary))',
+               foreground: 'hsl(var(--secondary-foreground))',
+            },
+            muted: {
+               DEFAULT: 'hsl(var(--muted))',
+               foreground: 'hsl(var(--muted-foreground))',
+            },
+            accent: {
+               DEFAULT: 'hsl(var(--accent))',
+               foreground: 'var(--accent-foreground)',
+            },
+            border: 'hsl(var(--border))',
+         },
+         fontSize: {
+            base: '18px',
+         },
+      },
+   },
+   plugins: [],
+};

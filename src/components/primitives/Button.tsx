552e65e5d7f26062eaf3aa652e0bba50f1454d9f
arundaya <runsworkspace@gmail.com>
2025-03-09 15:56:26 +0700

    chore: remove unecessary clsx

--- src/components/primitives/Button.tsx
+++ src/components/primitives/Button.tsx
@@ -1,34 +1,34 @@
-import React, { forwardRef } from 'react';
-import { cn } from '../../lib/utils';
-import clsx from 'clsx';
+import React, { forwardRef } from "react";
+import { cn } from "../../lib/utils";
+import clsx from "clsx";
 
 interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
-   className?: string;
-   type?: 'primary' | 'secondary';
-   isActive?: boolean;
+    className?: string;
+    type?: "primary" | "secondary";
+    isActive?: boolean;
 }
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-   ({ className, type = 'secondary', isActive, ...props }, ref) => {
-      return (
-         <button
-            className={cn(
-               clsx({
-                  'text-primary-foreground': isActive,
-                  'text-secondary-foreground': !isActive,
-                  'bg-primary': type === 'primary',
-                  'bg-secondary': type === 'secondary',
-                  'font-semibold': true,
-                  'hover:text-foreground': true,
-               }),
-               className
-            )}
-            ref={ref}
-            {...props}
-         />
-      );
-   }
+    ({ className, type = "secondary", isActive, ...props }, ref) => {
+        return (
+            <button
+                className={cn(
+                    {
+                        "text-primary-foreground": isActive,
+                        "text-secondary-foreground": !isActive,
+                        "bg-primary": type === "primary",
+                        "bg-secondary": type === "secondary",
+                        "font-semibold": true,
+                        "hover:text-foreground": true,
+                    },
+                    className,
+                )}
+                ref={ref}
+                {...props}
+            />
+        );
+    },
 );
 
-Button.displayName = 'Button';
+Button.displayName = "Button";
 export default Button;
